ba2e8bbdd773f54ac1ab942c74c5e479214d1ffc
Carlos Z <carloxz.17@gmail.com>
2020-07-09 22:33:18 -0400

    conecta a db en la nube

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost', 'root', 'root', 'tu_encuesta');
+    $conn = new mysqli('sql10.freesqldatabase.com', 'sql10353817', 'sSGtN7BDP3', 'sql10353817');
 
     if( $conn -> connect_error) {
         echo $error -> $conn -> connect_error;
