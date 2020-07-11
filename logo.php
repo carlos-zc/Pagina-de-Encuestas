808689e06aaac8acd62a6a3cf9abbf0fef6050cb
WillieApfel <williemr17@gmail.com>
2020-07-10 22:43:57 -0400

    Logo

---
+++ logo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Animación Logo</title>
+</head>
+<body>
+    <div class="contenedor">
+        <img class="ujaplogo" src="galeria/Logo-UJAP2.png" alt="Logo Ujap">
+    </div>
+</body>
+</html>
